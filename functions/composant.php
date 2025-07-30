b69a2565c100510f2c742c0012c5fc1da6de803a
nadiaouafi <nadiaouafi.mdgmail.comgit config --global user.email nadiaouafi.md@gmail.com>
2025-07-30 10:01:10 -0400

    Fusion des conflits tp2D

--- functions/composant.php
+++ functions/composant.php
@@ -24,7 +24,7 @@ function vague($couleur1) {
     echo '
     <div class="vague">
         <svg viewBox="0 0 1440 150" xmlns="http://www.w3.org/2000/svg">
-            <path fill="'.$couleur1.'">
+            <path fill="'.$couleur1. $couleur2.'">
                 <animate attributeName="d" dur="10s" repeatCount="indefinite"
                     values="
                         M0,40 C360,120 1080,-40 1440,40 L1440,0 L0,0 Z;
