dcfcf7150780c7fe2f0b3e604ae40a0d7cb69663
nadiaouafi <nadiaouafi.mdgmail.comgit config --global user.email nadiaouafi.md@gmail.com>
2025-08-22 10:37:04 -0400

    Mise à jour des fichiers

--- erreur404.php
+++ erreur404.php
@@ -10,11 +10,27 @@
 <?php get_header(); ?>
 
 <main class="error-404">
-    <h1 class="error-404__title">Oups ! Page introuvable</h1>
-    <p class="error-404__text">La page que vous cherchez n'existe pas ou a été déplacée.</p>
-    <a href="<?php echo home_url(); ?>" class="error-404__home">Retour à l’accueil</a>
+   <section class="erreur-404">
+  <div class="erreur-404__content">
+    <h1 class="erreur-404__title">
+      <?php echo get_theme_mod('section_404_title', 'Oups, page introuvable !'); ?>
+    </h1>
+    
+    <p class="erreur-404__message">
+      <?php echo get_theme_mod(
+        'section_404_message',
+        "Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. 
+        Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !"
+      ); ?>
+    </p>
 
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/404-illustration.svg" alt="Page 404" class="error-404__image">
+    <a href="<?php echo home_url(); ?>" class="erreur-404__btn">
+      Retour à l’accueil
+    </a>
+  </div>
+</section>
+
+    <img src="<?php echo get_template_directory_uri(); ?>/images/ilepalier.jpg" alt="Page 404" class="error-404__image">
 </main>
 
 <?php get_footer(); ?>
