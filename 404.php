147c643785b7ca2772adece444f6383b1a35f97e
nadiaouafi <nadiaouafi.mdgmail.comgit config --global user.email nadiaouafi.md@gmail.com>
2025-08-22 09:48:29 -0400

    Mise à jour des fichiers

--- 404.php
+++ 404.php
@@ -1,7 +1,21 @@
 <?php
 /**
- * Page 404
+ * Template 404
+ *
+ * @package club-voyage
  */
-get_header();
-get_template_part('gabarit/erreur', '404');
-get_footer();
+
+get_header(); // Inclut header.php
+?>
+
+<main id="site-content" class="erreur-404-page">
+
+    <?php
+    // Appel du gabarit pour la 404
+    get_template_part('gabarit/erreur', '404');
+    ?>
+
+</main>
+
+<?php
+get_footer(); // Inclut footer.php
